adc20012e2206a1bb7342b985bc268abe25a7353
Akash raj(CMD) <akashraj9828@gmail.com>
2020-08-02 14:40:59 +0530

    theme changes, Title change

--- src/redux/default_state.js
+++ src/redux/default_state.js
@@ -2,17 +2,11 @@ export const default_state = {
 	user: {
 		login: false,
 		userId: "",
-		responseCode: 404,
-		status: "",
-		customerCode: "",
-		firstName: "",
-		lastName: "",
 		role: "",
 		logo: "",
-		authToken: "",
 	},
 	app: {
-		theme: "light",
+		theme: "dark",
 		error: null,
 		help: false,
 		validationError: null,
