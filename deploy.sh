5f57813a827a3c9ea8d2379263fb061ab42e54c1
Akash raj(CMD) <akashraj9828@gmail.com>
2020-08-10 01:37:33 +0530

    switched to yarn

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 rm -rf build/
-npm install
-npm run build
+yarn install
+yarn build:snap
 openssl aes-256-cbc -a -d -in key.pem.enc -out key.pem -k $ENC_KEY
 chmod 600 key.pem
 scp -o  StrictHostKeyChecking=no -P 65002 -i key.pem  -r build/* $USER@$SERVER:domains/akashraj.tech/public_html/
